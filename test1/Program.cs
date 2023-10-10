45a76d4c71ec5e1c9b48b3976a8d2afceaa5b4cc
Pasha <Pasha@MATEBOOK16S>
2023-10-10 18:39:56 +0300

    Добавьте файлы проекта.

---
+++ test1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Design;
+
+namespace test1
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int b = 0;
+            int c = x * (-1);
+            if (x > b) 
+            {
+                Console.WriteLine($"Модуль = {x} ");
+            }
+            else
+                if (x < b)
+            {
+                Console.WriteLine($"Модуль = {c}");
+            }
+            else;
+        }
+    }
+}
